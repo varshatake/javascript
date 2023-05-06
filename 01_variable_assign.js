813ee022f1deb6a58fcd297045ebbd124c00698f
varshatake <varshagore108@gmail.com>
2023-05-06 07:28:38 +0530

    Variable Assignment_1
    Variable Assignment 1

---
+++ 01_variable_assign.js
@@ -0,0 +1,30 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Varsha Bharat Take";
+console.log(myFullName);
+
+var myLove;
+myLove = "I Love Only JS";
+console.log(myLove);
+
+var firstName = "Varsha";
+console.log(firstName);
+
+var lastName = "Take";
+console.log(lastName);
+
+var age = "29";
+console.log(age);
+
+var address = "A-401,Rudraksha society,Kate wasti,Punawale-411033";
+console.log(address);
+
+var pincode = "411033";
+console.log(pincode);
+
+var gender = "Female";
+console.log(gender);
+
+var isMarried = "Married";
+console.log(isMarried);
