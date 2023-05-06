0916abaa4f889086f4a1e71a16d2631c73ef1ba6
varshatake <varshagore108@gmail.com>
2023-05-06 17:02:17 +0530

    Assignment on Datatype_01

---
+++ 02_variableBasicsAssing.js
@@ -0,0 +1,31 @@
+var pune_PIN = 411033;
+var ahmednagar_PIN = 414604;
+var shiral_PIN = 414106;
+console.log("*********Assignment No. 1***********");
+console.log("----Number o/p-----");
+console.log("Pune PIN_CODE :",pune_PIN,"Type is:", typeof pune_PIN);
+console.log("Newasa PIN_CODE :",ahmednagar_PIN,"Type is:", typeof ahmednagar_PIN);
+console.log("Shiral PIN_CODE :",shiral_PIN,"Type is:", typeof shiral_PIN);
+
+var vname = "Varsha Take";
+var edu = "BE Computer";
+var hobby = "Like to draw Sketch";
+console.log("-----String o/p-----");
+console.log("Name is :",vname,"Type is:", typeof vname);
+console.log("Education:",edu,"Type is:", typeof edu);
+console.log("Hobby:",hobby,"Type is:", typeof hobby);
+
+var isMarried = true;
+var isFullstack = false;
+console.log("-----Boolean o/p-----");
+console.log("Married :",isMarried,"Type is:",typeof isMarried);
+console.log("Fullstack :",isFullstack,"Type is:",typeof isFullstack);
+
+// Difference between Null vs undefined
+var special = "These are special Data Types";
+var undefined = "A variable that is declared but we can not assign any value to that variable is called as 'undefined'.";
+var emptyvalue = "Null means it does not contain any value i.e. No value means call it as 'Null'.";
+console.log("-------" ,special ,"-------");
+console.log("Undefined :",undefined);
+console.log("Null :",emptyvalue);
+
---
+++ 03_typeOf.js
@@ -0,0 +1,23 @@
+var bankName;
+console.log("*******ASSIGNMENT NO. 2********");
+console.log("====Before Initialize value====");
+console.log(bankName);
+bankName = "SBI Bank";
+var marks = '90%';
+console.log("====After Initialize value====");
+console.log("Bank name is:",bankName,"Type of :" ,typeof bankName);
+console.log("Marks is:",marks,"Type is:",typeof marks);
+
+var isWorking;
+console.log("====Before Initialize====");
+console.log(isWorking);
+isWorking = true;
+console.log("====After Initialize====");
+console.log("Working :",isWorking,"Type of:" ,typeof isWorking);
+
+var totalCount = "Hundred and seven";
+console.log("****Total Count****");
+console.log("Total count :",totalCount,"Type is :" ,typeof totalCount);
+totalCount = 107;
+console.log("Updated total count is :", totalCount,"Type is :",typeof totalCount);
+
