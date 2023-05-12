c2f4ff8eb76a05c8e4362d40cc033340d7875249
varshatake <varshagore108@gmail.com>
2023-05-12 09:03:59 +0530

    Swap Variable
    Swap variable

---
+++ 02_swapVariables.js
@@ -0,0 +1,9 @@
+var num1 = 100;
+var num2 = 200;
+// Output --> num1 = 200,  num2 =100
+
+console.log("Before Swap..", "num1: ", num1, " num2: ", num2);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log("num1:", num1, " num2:", num2);
---
+++ 02_swapVaribale.js
@@ -0,0 +1,22 @@
+
+
+var num1 = 100;
+var num2 = 200;
+// Output --> num1 = 200,  num2 =100
+
+console.log("Before Swap..", "num1: ", num1, " num2: ", num2);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log("num1:", num1, " num2:", num2);
+
+var name1 = "Billgates";
+var name2 = "Elon Musk";
+console.log("Before Swap..", "name1: ", name1, " name2: ", name2);
+var temp = name1;
+name1 = name2;
+name2 = temp;
+console.log("After Swap", "name1:", name1, " name2:", name2);
+
+var cityPune = "Pune";
+var cityMumbai = "Mumbai";
