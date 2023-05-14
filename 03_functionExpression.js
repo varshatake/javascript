f2934b15525e6c7b454958397049604b49fed5a8
varshatake <varshagore108@gmail.com>
2023-05-14 17:52:01 +0530

    Class file function expression

---
+++ 03_functionExpression.js
@@ -0,0 +1,6 @@
+var multiplication = function (one, two, three) {
+    var result = one*two*three;
+    return `Multiplication is: ${result}`;
+}
+var result = multiplication(2, 2, 2 );
+console.log(result);
