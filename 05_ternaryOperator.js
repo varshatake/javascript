3cf949dddf19c6627a00adfdd8444f94a7602087
varshatake <varshagore108@gmail.com>
2023-05-16 14:38:14 +0530

    Class file Ternary Operator
    Ternary operator

---
+++ 05_ternaryOperator.js
@@ -0,0 +1,32 @@
+var isAssignmentDone = true;
+var result= isAssignmentDone ? " You will get job " :  "You will not get Job";
+console.log(result);
+
+var amarSSC = 57;
+var amarHSC = 89;
+var result = amarSSC>=55 && amarHSC >=60 ? " Allowed for interview" : "Sorry, Not allowed";
+console.log(`${result}`);
+
+var isAssignmentDone = true;
+var result= isAssignmentDone ? " You will get job " :  "You will not get Job";
+console.log(result);
+
+var amarSSC = 57;
+var amarHSC = 89;
+var result = amarSSC>=55 && amarHSC >=60 ? " Allowed for interview" : "Sorry, Not allowed";
+console.log(`${result}`);
+
+var rahulAge = 17;
+var result = rahulAge>=18 ? "You are eligible for vote" : "Not try next time";
+console.log(`${result}`);
+
+var candidateName = "Jenny";
+var age = 23
+var gender = "male";
+
+var result = (gender=="female" && age>=18) || (gender=="male" && age>=21)  ? "You are eligible" : "No you are not eligible";
+console.log(`${result}`);
+
+var num1 = 13;
+var result = num1%2 == 0 ? " EVEN number" : "ODD Number";
+console.log(`${result}`);
