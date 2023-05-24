1dcbb9958e164e225fcf05e60814b593c5f4a18e
varshatake <varshagore108@gmail.com>
2023-05-24 13:08:16 +0530

    Update 08_countchara0rA.js

--- 08_countchara0rA.js
+++ 08_countchara0rA.js
@@ -6,15 +6,17 @@ function countCharacter(strg) {
         totalchar = strg.charAt(index);
         if(totalchar=="a" || totalchar=="A") 
         {
-            console.log(totalchar);
+            //console.log(totalchar);
             ct++;
         }
     }
     return ct;
 }
 let givenstring1 = "I AM Learning Javascript, The language of internet.";
+console.log(`Given string 1 : ${givenstring1}`);
 let answer = countCharacter(givenstring1);
 console.log(`Character count for ['a' or 'A'] in String 1 : ${answer}`);
 let givenstring2 = "My favorite movie is LAggAn."
+console.log(`Given string 2 : ${givenstring2}`);
 answer = countCharacter(givenstring2);
 console.log(`Character count for ['a' or 'A'] in String 2 : ${answer}`);
