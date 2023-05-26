a297673790c5f3b22dbd75b6e6ee6d79c3a70f11
varshatake <varshagore108@gmail.com>
2023-05-26 14:10:36 +0530

    06 While loop Assignment
    Assignment while loop

--- 06_whileAssignment01.js
+++ 06_whileAssignment01.js
@@ -21,3 +21,40 @@ while (odd<=15) {
     }
     odd++;
 }
+
+console.log(`WAP to find numbers first 15 odd Numbers`);
+let even=0;
+while (even<=10) {
+    if (even%2==0) {
+        console.log(even);
+    }
+    even=even+1;
+}
+
+console.log(`WAP to print table of 5 like --> 5 10 15 20 25.....50`);
+let tableFive=5;
+while (tableFive<=50) {
+    if (tableFive%5==0) {
+        console.log(tableFive);
+    }
+    tableFive++;
+}
+
+console.log(`WAP to print table of 10 like --> 10 20 30.....100`);
+let tableTen = 10;
+while (tableTen<=100) {
+    if (tableTen%10==0) {
+        console.log(tableTen);
+    }
+    tableTen++;
+}
+
+console.log(`WAP to print table of 10 in Reverse Order like --> 100 90 80.....10`);
+let reverseTenTable =100;
+while (reverseTenTable>=10) {
+    if (reverseTenTable%10==0) {
+        console.log(reverseTenTable);
+    }
+    reverseTenTable--;
+}
+
