370d8305093731ae6efa9943a8bc96bcf28909a2
varshatake <varshagore108@gmail.com>
2023-06-14 07:54:21 +0530

    Dom assignment 12 June 23

---
+++ DOM/assignmentScript.js
@@ -0,0 +1,11 @@
+console.log(`---- Main heading ----`);
+const mainHeading = document.querySelector("h1");
+console.log(mainHeading.innerHTML);
+
+console.log(`---- Second Advantage ----------`);
+const secondAdvantage =document.querySelector("#avail");
+console.log(secondAdvantage.innerHTML);
+
+console.log(`------ Advantages ----------`);
+const advDiv = document.querySelector("#advantages");
+console.log(advDiv.innerHTML);
